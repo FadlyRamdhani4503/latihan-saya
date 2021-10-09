6426a3f2f5ff84dfe9ca68f9c0ea9b413e62f5ce
FadlyRamdhani4503 <fadlicaos@gmail.com>
2021-10-09 10:58:48 +0700

    Nama.java

---
+++ Nama.java
@@ -0,0 +1,10 @@
+public class Nama {
+	public static void main(String[] args) {
+		String nama = "Fadly Ramdhani";
+		int umur = 19;
+		System.out.println("Hallo, " + nama);
+		System.out.println("Ternyata Usiamu baru " + umur + " tahun ya!");
+		
+		}
+	
+	}
